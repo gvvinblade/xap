be6d396f5e4e6076262608e15ebc379673eb990e
nivin <niv@gigaspaces.com>
2021-06-08 09:22:35 +0300

    disable for troubleshooting

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/io/IOUtils.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/io/IOUtils.java
@@ -75,7 +75,7 @@ public class IOUtils {
     private static final IClassSerializer<?> _defaultSerializer = ObjectClassSerializer.instance;
     private static final IClassSerializer<?> _nullSerializer = new NullClassSerializer<>();
     private static final IClassSerializer<?> _smartExternalizableSerializer = SmartExternalizableSerializer.instance;
-    public static final boolean SMART_EXTERNALIZABLE_ENABLED = GsEnv.propertyBoolean(SystemProperties.SMART_EXTERNALIZABLE_ENABLED).get(true);
+    public static final boolean SMART_EXTERNALIZABLE_ENABLED = GsEnv.propertyBoolean(SystemProperties.SMART_EXTERNALIZABLE_ENABLED).get(false);
 
     static {
         _typeCache = new HashMap<Class<?>, IClassSerializer<?>>();
